b3e784c91972914051c39600a882652db6e97e02
Devlin <drcortens@gmail.com>
2022-09-21 17:39:07 +0100

    Update Navbar.js
    Added links that will be needed for the navbar

--- coffee-review-website/src/components/Navbar.js
+++ coffee-review-website/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faCoffee } from "@fortawesome/free-solid-svg-icons"
+import { faCoffee, faHome, faPerson } from "@fortawesome/free-solid-svg-icons"
 
 function Navbar() {
     return (
@@ -10,7 +10,16 @@ function Navbar() {
                 <div className='navbar-container'>
                     <Link to="/" className="navbar-image">
                         {/*This will be replaced with a logo later but placeholder text and icon for now*/}
-                        COFFEE RECIPES <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
+                        COFFEE RECIPES
+                    </Link>
+                    <Link to="/" className="navbar-image">
+                        Home <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
+                    </Link>
+                    <Link to="/recipes" className="navbar-image">
+                        Recipes <FontAwesomeIcon icon={faCoffee}></FontAwesomeIcon>
+                    </Link>
+                    <Link to="/profile" className="navbar-image">
+                        Profile <FontAwesomeIcon icon={faPerson}></FontAwesomeIcon>
                     </Link>
                 </div>
             </nav>

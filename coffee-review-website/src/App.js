1459b82c18a44caeb049777d62654c54c3ed3865
Devlin <drcortens@gmail.com>
2022-09-21 04:01:09 +0100

    Update App.js
    testing commits work when editing files

--- coffee-review-website/src/App.js
+++ coffee-review-website/src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br></br>
+        <p>
+          Testing and testing github commits!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"

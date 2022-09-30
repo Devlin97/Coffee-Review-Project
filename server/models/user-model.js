2fa0ae1084ec5c36ff88b62a9dcac7d35dde8148
Devlin <drcortens@gmail.com>
2022-10-01 00:46:04 +0100

    Update user-model.js

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,1 +1,11 @@
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    dateOfBirth: Date,
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

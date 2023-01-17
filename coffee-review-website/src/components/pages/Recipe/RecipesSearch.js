c508bbdc4d33ea13d9a52858b3d87a852c28de98
Devlin <drcortens@gmail.com>
2023-01-17 02:51:12 +0000

    Update RecipesSearch.js
    Inserted a search bar for recipes

--- coffee-review-website/src/components/pages/Recipe/RecipesSearch.js
+++ coffee-review-website/src/components/pages/Recipe/RecipesSearch.js
@@ -4,6 +4,7 @@ const RecipesSearch = ({recipesIn}) => {
 
   return (
     <>
+      <input type='text' placeholder='Search by Recipe Name or Brew Device...'/>
         {recipesIn.map(rec => (
             <p>{rec.title}</p>
         ))}

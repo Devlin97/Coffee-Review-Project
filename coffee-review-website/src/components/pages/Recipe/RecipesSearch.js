17e9421deebf97af48e4a7fafbbab13ab18cf526
Devlin <drcortens@gmail.com>
2023-01-19 15:29:00 +0000

    Update RecipesSearch.js
    Recipe dynamic populate list logic

--- coffee-review-website/src/components/pages/Recipe/RecipesSearch.js
+++ coffee-review-website/src/components/pages/Recipe/RecipesSearch.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
 
 const RecipesSearch = ({recipesIn}) => {
 
   return (
     <>
       <input type='text' placeholder='Search by Recipe Name or Brew Device...'/>
-        {recipesIn.map(rec => (
-            <p>{rec.title}</p>
-        ))}
+      <List>
+      {recipesIn.map(rec => (
+        <ListItem>
+          <ListItemButton>
+            <ListItemText primary={rec.title} />
+          </ListItemButton>
+        </ListItem>
+      ))}
+      </List>
+        
     </>
   )
 }

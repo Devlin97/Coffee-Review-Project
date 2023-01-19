8f3744a43c1ca59ba3bc7a4b4138553447221314
Devlin <drcortens@gmail.com>
2023-01-19 15:45:30 +0000

    Icons added, Search bar styled

--- coffee-review-website/src/components/pages/Recipe/RecipesSearch.js
+++ coffee-review-website/src/components/pages/Recipe/RecipesSearch.js
@@ -3,22 +3,40 @@ import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField';
+import Search from '@mui/icons-material/Search'
+import InputAdornment from '@mui/material/InputAdornment';
 
 const RecipesSearch = ({recipesIn}) => {
 
   return (
     <>
-      <input type='text' placeholder='Search by Recipe Name or Brew Device...'/>
-      <List>
-      {recipesIn.map(rec => (
-        <ListItem>
-          <ListItemButton>
-            <ListItemText primary={rec.title} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-      </List>
-        
+      <Box sx={{ bgcolor: '#6F4e37', color: 'whitesmoke', width: 400 }}>
+        <TextField 
+          id='recipe-search' 
+          label='Search' 
+          variant='standard'
+          placeholder='Search by Title or Brew Device...'
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <Search />
+              </InputAdornment>
+            )
+          }}
+          sx={{ width: '100%' }}
+        />
+        <List>
+        {recipesIn.map(rec => (
+          <ListItem>
+            <ListItemButton>
+              <ListItemText primary={rec.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+        </List>
+      </Box>
     </>
   )
 }

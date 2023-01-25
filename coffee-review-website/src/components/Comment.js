e758362d11c3ab5c453b21a99471a6997fa1e10b
Devlin <drcortens@gmail.com>
2023-01-25 15:44:31 +0000

    Update Comment.js
    Styling and input

--- coffee-review-website/src/components/Comment.js
+++ coffee-review-website/src/components/Comment.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import InputAdornment from '@mui/material/InputAdornment'
+import CommentIcon from '@mui/icons-material/Comment'
+import Stack from '@mui/material/Stack' 
+
+const textColor = '#CBCCCD';
 
 const Comment = ({ postIdIn }) => {
   const [comments, setComments] = useState([]);
@@ -34,20 +41,50 @@ const Comment = ({ postIdIn }) => {
 
   if(comments.length > 0) {
   return (
-    <>
-      {comments.map(com => (
-        <Card variant='outline' key={com.id}>
-          <CardContent>
-              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                  {com.name}
-              </Typography>
-              <Typography variant="h5" component="div">
-                  {com.text}
-              </Typography>
-          </CardContent>
-        </Card>
-      ))}
-    </>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '0', marginTop: '10px' }}>
+      <Stack
+        spacing={2}
+        direction='column'
+        alignItems='stretch'
+        width='100%'
+        sx={{
+          maxWidth: '300px'
+        }}
+      >
+        <TextField 
+          id='recipe-search' 
+          label='Search' 
+          variant='standard'
+          multiline
+          rows={3}
+          placeholder='Leave a Comment for this Recipe!'
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <CommentIcon style={{ color: '#CBCCCD' }} />
+              </InputAdornment>
+            )
+          }}
+          sx={{ width: '100%', input: { color: textColor } }}
+          inputProps={{ style: { color: textColor } }}
+          InputLabelProps={{ style: { color: '#CBCCCD' } }}
+        />
+        {comments.map(com => (
+          <Box sx={{ minWidth: '250px' }}>
+            <Card variant='outline' key={com.id} sx={{ background: 'linear-gradient( 112.1deg,  rgba(32,38,57,0.6) 11.4%, rgba(63,76,119,0.6) 70.2% )'}}>
+              <CardContent>
+                  <Typography sx={{ fontSize: 14 }} color={textColor} gutterBottom>
+                      {com.name}
+                  </Typography>
+                  <Typography variant="h5" component="div" color={textColor}>
+                      {com.text}
+                  </Typography>
+              </CardContent>
+            </Card>
+          </Box>
+        ))}
+      </Stack>
+    </Box>
   )}
 }
 

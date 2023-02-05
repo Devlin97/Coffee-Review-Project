7961b01ed68edcf900b86a79bfb3c5634ff3c7f2
Devlin <drcortens@gmail.com>
2023-02-05 22:18:29 +0000

    Update Home.js
    Styling

--- coffee-review-website/src/components/pages/Home.js
+++ coffee-review-website/src/components/pages/Home.js
@@ -1,11 +1,21 @@
 import '../../App.css';
 import Paper from '@mui/material/Paper'
 import Box from '@mui/material/Box'
+import { color } from '@mui/system';
 
 function Home() {
     return (
         <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
-            <Paper elevation={3} sx={{ maxWidth: '550px', width: '100%', padding: '10px', backgroundColor: '#6F4e37' }}>
+            <Paper 
+                elevation={3} 
+                sx={{ 
+                    maxWidth: '550px', 
+                    width: '100%', 
+                    padding: '10px', 
+                    background: 'linear-gradient( 112.1deg,  rgba(32,38,57,0.6) 11.4%, rgba(63,76,119,0.6) 70.2% )',
+                    color: '#CBCCCD' 
+                }}>
+
                 <h1 style={{ textAlign: 'center' }}>
                     Welcome to Third Wave Coffee Review
                 </h1>

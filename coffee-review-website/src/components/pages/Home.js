923dc92ea601fbe36a7827be90cc24986f2457c5
Devlin <drcortens@gmail.com>
2023-02-05 21:40:59 +0000

    Boilerplate home page

--- coffee-review-website/src/components/pages/Home.js
+++ coffee-review-website/src/components/pages/Home.js
@@ -1,12 +1,16 @@
 import '../../App.css';
+import Paper from '@mui/material/Paper'
+import Box from '@mui/material/Box'
 
 function Home() {
     return (
-        <>
-            <h1>
-                TEST HOME
-            </h1>
-        </>
+        <Box sx={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}>
+            <Paper elevation={3} sx={{ maxWidth: '550px', width: '100%', padding: '10px', backgroundColor: '#6F4e37' }}>
+                <h1 style={{ textAlign: 'center' }}>
+                    Welcome to Third Wave Coffee Review
+                </h1>
+            </Paper>
+        </Box>
     )
 }
 

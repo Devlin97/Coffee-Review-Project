9d76a9c92cbe082d9c301343a96733f305fa6179
Devlin <drcortens@gmail.com>
2023-02-07 15:50:59 +0000

    Create recipe-model-pourover.js
    This is a model for a pourover recipe. It's the same as the base ('immersion') recipe, except it has a pours, bloom time, and bloom weight attributes

---
+++ server/models/recipe-model-pourover.js
@@ -0,0 +1,66 @@
+import { Sequelize, DataTypes } from 'sequelize';
+import { sequelize } from '../database/db.js';
+
+const Recipe_Pourover = sequelize.define(
+    'Recipe_Pourover',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        brewMethod: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        coffeeWeight: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        waterWeight: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        },
+        brewer: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        grinder: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        grindSize: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        coffeeOrigin: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        pours: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        bloomTime: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        bloomWeight: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        totalTimeMinutes: {
+            type: DataTypes.DECIMAL,
+            allowNull: false
+        }
+    });
+
+export default Recipe_Pourover;

1410c606c7624c3aff15ae65cb4a1a2d2dc8591b
Devlin <drcortens@gmail.com>
2023-02-22 21:49:18 +0000

    What is pourover styling

--- coffee-review-website/src/components/pages/About/AboutList.js
+++ coffee-review-website/src/components/pages/About/AboutList.js
@@ -2,54 +2,61 @@ import React, { useState } from 'react'
 import { List, ListItem, ListItemButton, ListSubheader, ListItemText, Collapse } from '@mui/material'
 import ExpandLess from '@mui/icons-material/ExpandLess'
 import ExpandMore from '@mui/icons-material/ExpandMore'
+import WhatIsPourover from './WhatIsPourover'
+import { Stack } from '@mui/system'
 
 const AboutList = () => {
     const [open, setOpen] = useState(true)
     const [explainer, setExplainer] = useState()
 
   return (
-    <List sx={{ 
-        height: '100%', 
-        maxHeight: '550px', 
-        overflowY: 'auto', 
-        maxWidth: 400, 
-        backgroundColor: 'rgba(63,76,119,0.2)',
-        color: '#CBCCCD' 
-        }} 
-        subheader={
-        <ListSubheader sx={{ 
-            color: '#CBCCCD',
-            backgroundColor: 'rgba(63,76,119,0.2)'  
-            }}>
-            Learn
-        </ListSubheader>}>
+    <>
+    <Stack direction='row'>
+        <List sx={{ 
+            height: '100%', 
+            maxHeight: '550px', 
+            overflowY: 'auto', 
+            maxWidth: 400, 
+            backgroundColor: 'rgba(63,76,119,0.2)',
+            color: '#CBCCCD' 
+            }} 
+            subheader={
+            <ListSubheader sx={{ 
+                color: '#CBCCCD',
+                backgroundColor: 'rgba(63,76,119,0.2)'  
+                }}>
+                Learn
+            </ListSubheader>}>
 
-        <ListItem>
-            <ListItemButton onClick={() => {setOpen(!open)}}>
-                <ListItemText primary='Ways to brew speciality coffee' />
-                {open ? <ExpandLess /> : <ExpandMore />}
-            </ListItemButton>
-        </ListItem>
-            <Collapse in={open} timeout='auto' unmountOnExit>
-                <List disablePadding>
-                    <ListItem sx={{ pl: 4 }}>
-                        <ListItemButton>
-                            <ListItemText primary='Pourover' />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem sx={{ pl: 4 }}>
-                        <ListItemButton>
-                            <ListItemText primary='Immersion' />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem sx={{ pl: 4 }}>
-                        <ListItemButton>
-                            <ListItemText primary='Aeropress' />
-                        </ListItemButton>
-                    </ListItem>
-                </List>
-            </Collapse>
-    </List>
+            <ListItem>
+                <ListItemButton onClick={() => {setOpen(!open)}}>
+                    <ListItemText primary='Ways to brew speciality coffee' />
+                    {open ? <ExpandLess /> : <ExpandMore />}
+                </ListItemButton>
+            </ListItem>
+                <Collapse in={open} timeout='auto' unmountOnExit>
+                    <List disablePadding>
+                        <ListItem sx={{ pl: 4 }}>
+                            <ListItemButton>
+                                <ListItemText primary='Pourover' />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem sx={{ pl: 4 }}>
+                            <ListItemButton>
+                                <ListItemText primary='Immersion' />
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem sx={{ pl: 4 }}>
+                            <ListItemButton>
+                                <ListItemText primary='Aeropress' />
+                            </ListItemButton>
+                        </ListItem>
+                    </List>
+                </Collapse>
+        </List>
+        <WhatIsPourover />
+    </Stack>
+    </>
   )
 }
 
---
+++ coffee-review-website/src/components/pages/About/WhatIsPourover.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Stack } from '@mui/material'
+
+const WhatIsPourover = () => {
+  return (
+    <div className='about-holder'> 
+        <Stack>
+            <h1>Pourover</h1>
+            <h4 className='about-subheading'>What is it?</h4>
+            <p>Pourover is one of the most popular ways to brew coffee.</p>
+            <p>It is very fun</p>
+        </Stack>
+    </div> 
+  )
+}
+
+export default WhatIsPourover

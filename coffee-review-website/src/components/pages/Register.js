5cb90495127605ee8f4a4587613a73461c49ce13
Devlin <drcortens@gmail.com>
2023-02-24 12:56:33 +0000

    Conditional register if already logged in

--- coffee-review-website/src/components/pages/Register.js
+++ coffee-review-website/src/components/pages/Register.js
@@ -8,6 +8,15 @@ const Register = () => {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
 
+    const [isLoggedIn, setIsLoggedIn] = useState(() => {
+      if(localStorage.getItem('loginID') === null) {
+          return false;
+      }
+      else {
+          return true;
+      }
+    })
+
     const handleSubmit = async (e) => {
       e.preventDefault();
 
@@ -47,6 +56,7 @@ const Register = () => {
     
     }
 
+    if(!isLoggedIn) {
   return (
     <form onSubmit={handleSubmit}>
 
@@ -109,6 +119,33 @@ const Register = () => {
 
     </form>
   )
+    }
+    else {
+      return (
+        <Box sx={{ width: '100%', height: 600 , justifyContent: 'center', display: 'flex', alignItems: 'center' }}>
+
+            <Stack
+                spacing={4}
+                direction='column'
+                alignItems='center'
+                sx={{ 
+                    background: 'linear-gradient( 112.1deg,  rgba(32,38,57,0.6) 11.4%, rgba(63,76,119,0.6) 70.2% )', 
+                    paddingLeft: '100px',
+                    paddingRight: '100px',
+                    paddingBottom: '50px',
+                    paddingTop: '50px', 
+                    borderRadius: '25px' 
+                }}>
+
+                <h2 className='sign-in-header'>YOU ARE ALREADY LOGGED IN</h2>
+
+                <h2 className='sign-in-header'>THANK YOU FOR REGISTERING</h2>
+
+            </Stack>
+
+        </Box>
+      )
+    }
 }
 
 export default Register;
